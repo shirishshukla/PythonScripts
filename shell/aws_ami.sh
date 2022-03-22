f79c13f804d0062030e9943f83f3b07efa7ba103
Shirish Shukla <shirish.linux@gmail.com>
2022-03-23 00:21:56 +0530

    update by shirish @03/23/22-00:21:56-AM

--- shell/aws_ami.sh
+++ shell/aws_ami.sh
@@ -16,23 +16,25 @@ instanceID=$1
 
 TODAY=`date +%Y%m%d.%H%M`
 
-echo -e "----------------------------- $TODAY ---------------------------------"
-instance_name=$(aws ec2 describe-instances --instance-ids $instanceID --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value[]]' --output text)
-if [[ $instance_name == "" ]]; then
-    echo -e "Instance-ID ($instanceID) doesn't exist. Please check."
-    exit
+echo -e "-------------- Creating AMI of ec2 instance $instanceID --------------"
+if `aws ec2 describe-instances --instance-ids $instanceID >/dev/null 2>&1`; then
+    instance_name=$(aws ec2 describe-instances --instance-ids $instanceID --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value[]]' --output text)
+    [[ -z $instance_name ]] && instance_name=$instanceID
 else
-    #ami name
+    echo -e "Instance-ID ($instanceID) doesn't exist. Please check."
+    exit 1
+else
+    # ami name
     ami_name="${instance_name}-${TODAY}"
 
-    #create AMI from the instance
+    # create AMI from the instance
     ami_id=$(aws ec2 create-image --instance-id "$instanceID" --name "$ami_name" --description "Auto AMI from $instance_name ($instanceID)" --no-reboot --output text)
 
-    #Tag the AMI with instance id and creation date
+    # Tag the AMI with instance id and creation date
     if [[ ! -z $ami_id ]]; then
         echo -e "\n==> AMI: $ami_id ($ami_name) created successfully from $instance_name ($instanceID).\n"
         aws ec2 create-tags --resources $ami_id --tags Key=Name,Value=$ami_name Key=Instance_id,Value=$instanceID Key=AMI_Creation_Date,Value=$TODAY
     else
-        echo -e "\n==> FAILED: AMI creation failed from $instance_name ($instanceID). Please check.\n"
+        echo -e "\n==> FAILED: AMI creation failed for instance $instance_name ($instanceID). Please check.\n"
     fi
 fi
