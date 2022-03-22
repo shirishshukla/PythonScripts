57fc76c66ae5e6ca7a8a09bf9fe9deef8cb1cf74
Shirish Shukla <shirish.linux@gmail.com>
2022-03-23 00:03:30 +0530

    update by shirish @03/23/22-00:03:29-AM

--- shell/aws_ami.sh
+++ shell/aws_ami.sh
@@ -1,6 +1,6 @@
-## 
-# description: Create ami of instance 
-# this will create ami with 
+##
+# description: Create ami of instance
+# this will create ami with
 # name: instance_name-YYYMMDD.HHMM
 #### howto
 # sh script.sh instance-id
@@ -14,23 +14,23 @@ instanceID=$1
 
 TODAY=`date +%Y%m%d.%H%M`
 
-echo -e "----------------------------------\n  $TODAY  \n----------------------------------"
+echo -e "----------------------------- $TODAY ---------------------------------"
 instance_name=$(aws ec2 describe-instances --instance-ids $instanceID --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value[]]' --output text)
 if [[ $instance_name == "" ]]; then
-    echo -e "Instance-ID ($instance_id) doesn't exist. Please check." 
+    echo -e "Instance-ID ($instanceID) doesn't exist. Please check."
     exit
 else
     #Create the AMI name.
     ami_name="${instance_name}-${TODAY}"
 
     #To create AMI from the instance
-    ami_id=$(aws ec2 create-image --instance-id "$instance_id" --name "$ami_name" --description "Auto AMI from $instance_name ($instance_id)" --no-reboot --output text)
+    ami_id=$(aws ec2 create-image --instance-id "$instanceID" --name "$ami_name" --description "Auto AMI from $instance_name ($instanceID)" --no-reboot --output text)
 
     #Tag the AMI
     if [[ $ami_id != "" ]]; then
-        echo -e "$ami_id ($ami_name) created successfully from $instance_name ($instance_id).\n"
-        aws ec2 create-tags --resources $ami_id --tags Key=Instance_id,Value=$instance_id Key=AMI_Creation_Date,Value=$TODAY
+        echo -e "$ami_id ($ami_name) created successfully from $instance_name ($instanceID).\n"
+        aws ec2 create-tags --resources $ami_id --tags Key=Instance_id,Value=$instanceID Key=AMI_Creation_Date,Value=$TODAY
     else
-        echo -e "AMI creation failed from $instance_name ($instance_id). Please check.\n"
+        echo -e "AMI creation failed from $instance_name ($instanceID). Please check.\n"
     fi
 fi
