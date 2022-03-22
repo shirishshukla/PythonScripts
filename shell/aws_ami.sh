f3aad5e98c783e4f51a43482130cd597f9925246
Shirish Shukla <shirish.linux@gmail.com>
2022-03-23 00:13:16 +0530

    update by shirish @03/23/22-00:13:16-AM

--- shell/aws_ami.sh
+++ shell/aws_ami.sh
@@ -2,15 +2,17 @@
 # description: Create ami of instance
 # this will create ami with
 # name: instance_name-YYYMMDD.HHMM
-#### howto
+# Instance_id:
+# AMI_Creation_Date:
+#
+#### !! How TO!!
 # sh script.sh instance-id
 # eg.
 # sh script.sh i-234567689
-
 ##
 
 instanceID=$1
-[[ -z $instanceID ]] && echo "Please pass instance id as arg" && exit 1
+[[ -z $instanceID ]] && echo "Please pass instance-id as arg" && exit 1
 
 TODAY=`date +%Y%m%d.%H%M`
 
@@ -26,10 +28,10 @@ else
     #create AMI from the instance
     ami_id=$(aws ec2 create-image --instance-id "$instanceID" --name "$ami_name" --description "Auto AMI from $instance_name ($instanceID)" --no-reboot --output text)
 
-    #Tag the AMI with instance id and creation date 
+    #Tag the AMI with instance id and creation date
     if [[ ! -z $ami_id ]]; then
         echo -e "\n==> AMI: $ami_id ($ami_name) created successfully from $instance_name ($instanceID).\n"
-        aws ec2 create-tags --resources $ami_id --tags Key=Instance_id,Value=$instanceID Key=AMI_Creation_Date,Value=$TODAY
+        aws ec2 create-tags --resources $ami_id --tags Key=Name,Value=$ami_name Key=Instance_id,Value=$instanceID Key=AMI_Creation_Date,Value=$TODAY
     else
         echo -e "\n==> FAILED: AMI creation failed from $instance_name ($instanceID). Please check.\n"
     fi
